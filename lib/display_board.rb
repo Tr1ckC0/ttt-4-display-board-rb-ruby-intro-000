bf9ac8b1c4bbec0dab498e6601b6a3af91763e0e
Patrick Colina <trick.co@yahoo.com>
2019-12-05 01:40:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,12 @@
 board = [" "," "," "," "," "," "," "," "," "]# out the current state.
+border = "-----------"
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts border
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts border
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+end
+
+display_board(board)
